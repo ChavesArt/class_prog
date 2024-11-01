5b49feda0990f6c708ac70bda79f2bbc72a90913
ChavesArt <116437988+ChavesArt@users.noreply.github.com>
2024-11-01 08:08:22 -0300

    fsd

--- att_avaliativo/index.php
+++ att_avaliativo/index.php
@@ -7,9 +7,7 @@ include_once ("moto.php");
 $c1 = new carro(4,'Toyota','Corolla',2020);
 echo "<b>Carro</b><br>";
 $c1->exibirInformacoes();
-
 echo "<hr>";
-
 $m1 = new moto('689 cc','Yamaha','MT-07',2021);
 echo "<b>Moto</b><br>";
 $m1->exibirInformacoes();
